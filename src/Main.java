b193dce75289518f8e16ec71085aa732b235ac23
Calyen93 <genco.claudio3@gmail.com>
2024-02-28 15:55:56 +0100

    Main update

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setHunger(70);
 
+        // Creazione di un cibo e consumo da parte della persona
+        Food food = new Food("Pizza", 30);
+        food.consume(person);
+        System.out.println("Hunger dopo aver mangiato: " + person.getHunger());
+
+        // Creazione di una bevanda e consumo da parte della persona
+        Beverage beverage = new Beverage("Water", 20);
+        beverage.consume(person);
+        System.out.println("Thirst dopo aver bevuto: " + person.getThirst());
+    }
 }
